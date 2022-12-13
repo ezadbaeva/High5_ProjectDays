0cfc06ad717f64a63ff0d918436ba8dde8e4fbae
unknown <ezadbaeva@gmail.com>
2022-12-12 20:36:55 -0500

    hello people

---
+++ src/test/java/BestBuyApp.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BestBuyApp {
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+
+    @BeforeMethod
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.bestbuy.com/");
+    }
+    @Test
+    void validateCard(){
+
+        driver.findElement()
+    }
+}
